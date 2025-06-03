4b37c43f73ebc9492332a7e849a871e99834b3b6
Bhargav-Shivakumar <bhargavshiv23@gmail.com>
2025-06-03 14:40:12 +0100

    got mqtt working for pico and fast api server communication

---
+++ smart-grid/python_server/fastapi_client.py
@@ -0,0 +1,44 @@
+import paho.mqtt.client as mqtt
+import threading
+import time
+
+stop_event = threading.Event()
+broker = "192.168.135.60"
+port = 1883
+client_id   = "fastapi"
+topic_from_pico = "pico_data"
+topic_to_pico = "server_data"
+
+payload = "hello from server"
+
+def on_connect(client, *_):
+    client.subscribe(topic_from_pico)
+    print("connected to MQTT")
+
+def on_message(client, userdata, msg):
+    print(f"from pico: {msg.payload.decode()}")
+    # need to do something with this
+
+mqttc = mqtt.Client(client_id=client_id, protocol=mqtt.MQTTv311)
+mqttc.on_connect = on_connect
+mqttc.on_message = on_message
+mqttc.connect(broker, 1883, keepalive=60)
+
+def publish_to_pico():
+    while not stop_event.is_set():
+        mqttc.publish(topic_to_pico, payload, retain=False)
+        print(f"sent to pico: {payload}")
+        time.sleep(5)
+
+mqttc.loop_start()
+publisher = threading.Thread(target=publish_to_pico).start()
+
+try:
+    while True:
+        time.sleep(1)
+except KeyboardInterrupt:
+    print("Stopping...")
+    stop_event.set()  
+    publisher.join()
+    mqttc.loop_stop()
+    print("Stopped")
---
+++ smart-grid/python_server/pico_client.py
@@ -0,0 +1,31 @@
+import time
+#import ubinascii
+#import machine
+from umqtt.simple import MQTTClient
+
+client_id = "pico" # ubinascii.hexlify(machine.unique_id()) to allocate id
+broker = "192.168.135.60" # put your ip address in here as for now mosquitto is running locally
+port = 1883
+topic_from_server = b"server_data" # b has to be there as micropython requres byte string for mqtt topics
+topic_to_server = b"pico_data"
+
+payload = "hello from pico"
+
+def on_message(topic, msg):
+    print(f"from server: {msg.decode()}")
+    # need to do something with this
+
+mqttc = MQTTClient(client_id, broker)
+mqttc.set_callback(on_message)
+mqttc.connect()
+mqttc.subscribe(topic_from_server)
+print("connected to mqtt")
+
+def publish_to_server():
+    while True:
+        mqttc.publish(topic_to_server, payload)
+        print(f"sent to server: {payload}")
+        mqttc.check_msg()
+        time.sleep(5)
+
+publish_to_server()
