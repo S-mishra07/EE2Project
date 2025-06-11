37dd03c0253b222ea31254fb7fb9d60665837ecd
Bhargav-Shivakumar <bhargavshiv23@gmail.com>
2025-06-11 16:49:03 +0100

    added other leds

--- smart-grid/python_server/fastapi_client.py
+++ smart-grid/python_server/fastapi_client.py
@@ -10,7 +10,10 @@ from bson import ObjectId
 broker = "18.210.146.176"
 port = 1883
 client_id = "fastapi"
-topic_from_pico = "pico_data"
+topic_from_yellow = "yellow_data"
+topic_from_red = "red_data"
+topic_from_red2 = "red2_data"
+topic_from_blue = "blue_data"
 topic_to_led = "demand_data" 
 topic_to_PV  = "sun_data"
 
@@ -18,7 +21,10 @@ mongo_url = "mongodb+srv://akarshgopalam:bharadwaj@smart-grid.wnctwen.mongodb.ne
 client_to_pico = MongoClient(mongo_url)
 client_from_pico = MongoClient(mongo_url)
 db_from_pico = client_from_pico["test_pico"]
-collection_from_pico = db_from_pico["pico_messages"]
+collection_from_yellow = db_from_pico["pico_yellow"]
+collection_from_red = db_from_pico["pico_red"]
+collection_from_red2 = db_from_pico["pico_red2"]
+collection_from_blue = db_from_pico["pico_blue"]
 db_to_pico = client_to_pico["test"]
 collection_to_pico = db_to_pico["combined_ticks"]
 
@@ -29,32 +35,87 @@ def get_money(Vout, Iout):
     return money
 
 def on_connect(client, *_):
-    client.subscribe(topic_from_pico)
+    client.subscribe(topic_from_yellow)
+    client.subscribe(topic_from_red)
+    client.subscribe(topic_from_red2)
+    client.subscribe(topic_from_blue)
     print("Connected to MQTT broker")
 
 def on_message(client, userdata, msg):
+
     data = collection_to_pico.find_one(sort=[("_id", -1)], projection={"tick": 1, "_id": 0})
-    message = json.loads(msg.payload.decode())
-    print(f" from pico: {message}")
-    Vout = message["Vout"]
-    Iout = message["Iout"]
-    money = get_money(Vout, Iout)
-    
 
-    doc = {
+    if msg.topic == topic_from_yellow:
+        yellow_message = json.loads(msg.payload.decode())
+        print(f" from yellow LED: {yellow_message}")
+
+        doc = {
         "tick": data["tick"],
-        "Vin": message["Vin"],
-        "Vout": message["Vout"],
-        "Iout": message["Iout"],
-        "power": float(Vout) * float(Iout),
-        "money": money
-    }
-    try:
-        result = collection_from_pico.insert_one(doc)
-        print(f" Saved to MongoDB with _id: {result.inserted_id}")
-    except Exception as e:
-        print(f"MongoDB insert error: {e}")
+        "Vin": yellow_message["Vin"],
+        "Vout": yellow_message["Vout"],
+        "Iout": yellow_message["Iout"],
+        "power": float(yellow_message["Vout"]) * float(yellow_message["Iout"]),
+        }
 
+        try:
+            result = collection_from_yellow.insert_one(doc)
+            print(f" Saved to MongoDB with _id: {result.inserted_id}")
+        except Exception as e:
+            print(f"MongoDB insert error: {e}")
+        
+    if msg.topic == topic_from_red:
+        red_message = json.loads(msg.payload.decode())
+        print(f" from red LED: {red_message}")
+
+        doc = {
+        "tick": data["tick"],
+        "Vin": red_message["Vin"],
+        "Vout": red_message["Vout"],
+        "Iout": red_message["Iout"],
+        "power": float(red_message["Vout"]) * float(red_message["Iout"]),
+        }
+
+        try:
+            result = collection_from_red.insert_one(doc)
+            print(f" Saved to MongoDB with _id: {result.inserted_id}")
+        except Exception as e:
+            print(f"MongoDB insert error: {e}")
+    
+    if msg.topic == topic_from_red2:
+        red2_message = json.loads(msg.payload.decode())
+        print(f" from red LED: {red2_message}")
+
+        doc = {
+        "tick": data["tick"],
+        "Vin": red2_message["Vin"],
+        "Vout": red2_message["Vout"],
+        "Iout": red2_message["Iout"],
+        "power": float(red2_message["Vout"]) * float(red2_message["Iout"]),
+        }
+
+        try:
+            result = collection_from_red2.insert_one(doc)
+            print(f" Saved to MongoDB with _id: {result.inserted_id}")
+        except Exception as e:
+            print(f"MongoDB insert error: {e}")
+    
+    if msg.topic == topic_from_blue:
+        blue_message = json.loads(msg.payload.decode())
+        print(f" from red LED: {blue_message}")
+
+        doc = {
+        "tick": data["tick"],
+        "Vin": blue_message["Vin"],
+        "Vout": blue_message["Vout"],
+        "Iout": blue_message["Iout"],
+        "power": float(blue_message["Vout"]) * float(blue_message["Iout"]),
+        }
+
+        try:
+            result = collection_from_blue.insert_one(doc)
+            print(f" Saved to MongoDB with _id: {result.inserted_id}")
+        except Exception as e:
+            print(f"MongoDB insert error: {e}")
 
 mqttc = mqtt.Client(client_id=client_id, protocol=mqtt.MQTTv311)
 mqttc.on_connect = on_connect
